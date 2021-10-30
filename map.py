bff0036703d4b767d586cb4682b546c1a106d08f
nikhilcad <nikhilcaddilac@gmail.com>
2021-10-30 19:54:55 +0530

    WOrking plot even though its test

--- map.py
+++ map.py
@@ -1,6 +1,10 @@
 import geopandas as gpd
+import matplotlib.pyplot as plt
 
 fp ="L2_data/NLS/2018/L4/L41/L4132R.shp\m_L4132R_p.shp"
 
 data = gpd.read_file(fp)
 print(data.head())
+
+data.plot()
+plt.show()
